b112bf5ba6047286b048940237cea448a25241f4
Michal J. Gasior <michaljgasior@gmail.com>
2020-09-11 00:07:18 +0200

    Setup script

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # cracker-certbot
-Lightsail and HTTPS with Certbot
+
+## Lightsail and HTTPS with Certbot
+
+Setup script deletes old instance but doesn not delete the static IP which is used for the new instance.
+
+[Certification instructions](https://www.humankode.com/ssl/how-to-set-up-free-ssl-certificates-from-lets-encrypt-using-docker-and-nginx)
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,35 +1,10 @@
 echo -e "\e[93;104mCracker app scripts\e[0m\n"
 
-docker rm $(docker ps -a -q)
+echo -e "\n\e[92mDeleting instance\e[0m"
+aws lightsail delete-instance --instance-name Cracker-app
 
-echo -e "\n\e[92mAllocating new static IP\e[0m"
-aws lightsail allocate-static-ip --static-ip-name Cracker-app-ip
+echo -e "\n\e[92mGetting the allocated static IP\e[0m"
 aws lightsail get-static-ip --static-ip-name Cracker-app-ip
-
-read -p "Enter allocated static IP: " static_ip
-
-echo "Static IP: $static_ip"
-
-echo "REACT_APP_API_URL=https://$static_ip/api
-REACT_APP_AUTH0_ORIGIN=https://$static_ip" >> ".env.production"
-
-cp ".env.production" "./cracker-client/"
-
-# Build, tag and push new images
-echo -e "\n\e[92mBuild new images\e[0m"
-docker-compose -f docker-compose.prod.yml build
-
-echo -e "\n\e[92mTag cracker-server\e[0m"
-docker tag cracker-app_cracker-server mjgasior/cracker-server:0.0.1
-
-echo -e "\n\e[92mPush cracker-client\e[0m"
-docker push mjgasior/cracker-server:0.0.1
-
-echo -e "\n\e[92mTag cracker-client\e[0m"
-docker tag cracker-app_cracker-client mjgasior/cracker-client:0.0.3
-
-echo -e "\n\e[92mPush cracker-client\e[0m"
-docker push mjgasior/cracker-client:0.0.3
 
 # Create new instance
 echo -e "\n\e[92mCreating new instance\e[0m"
@@ -68,12 +43,8 @@ if [ $n -gt 6 ]; then
     exit
 fi
 
-echo -e "\n\e[92mAttaching new static IP to the instance\e[0m"
+echo -e "\n\e[92mAttaching static IP to the new instance\e[0m"
 aws lightsail attach-static-ip --static-ip-name Cracker-app-ip --instance-name Cracker-app
-
-# Remove configuration files
-rm ".env.production"
-rm "./cracker-client/.env.production"
 
 echo -e "\e[96mPress any key to exit...\e[0m"
 read
